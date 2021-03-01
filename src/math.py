aab0d399702a10be189e1ac8004cbbd57de5e533
Roman <romikor1999@gmail.com>
2021-03-01 18:27:02 +0100

    Math library structure.

---
+++ src/math.py
@@ -0,0 +1,57 @@
+"""@package docstring
+Custom math library for IVS project.
+"""
+
+
+def add(a, b):
+    """The function returns the value a plus b.
+       """
+    pass
+
+
+def sub(a, b):
+    """The function returns the value a minus b
+       """
+    pass
+
+
+def div(a, b):
+    """The function returns the value a divided by b
+       """
+    pass
+
+
+def multiplication(a, b):
+    """The function returns the value of a multiplied by b
+       """
+    pass
+
+
+def exp(a, b):
+    """TheThe function returns the value a raised to the power of b
+       """
+    pass
+
+
+def sqrt(x, n):
+    """the function returns the nth root of the x value
+       """
+    pass
+
+
+def fact(x):
+    """The function returns factorial of x
+       """
+    pass
+
+
+def cos(x):
+    """The function returns cosinus of x
+       """
+    pass
+
+
+def sin(x):
+    """The function returns sinus of x
+       """
+    pass
