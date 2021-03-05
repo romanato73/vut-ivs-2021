d84b4bb542568ad48bca3af7f0145bd2128e4de7
AntoninJarolim <jarolim.antonin@seznam.cz>
2021-03-06 00:11:50 +0100

    added simple math functions

--- src/mathstuck/math.py
+++ src/mathstuck/math.py
@@ -6,43 +6,68 @@ Custom math library for IVS project.
 def add(a, b):
     """The function returns the value a plus b.
        """
-    pass
+    return a + b
 
 
 def sub(a, b):
     """The function returns the value a minus b
        """
-    pass
+    return a - b
 
 
 def div(a, b):
     """The function returns the value a divided by b
        """
-    pass
+    if b == 0:
+        ValueError("DividedByZero")
+        return
+    return a / b
 
 
 def mul(a, b):
     """The function returns the value of a multiplied by b
        """
-    pass
+    return a * b
 
 
 def exp(a, b):
     """TheThe function returns the value a raised to the power of b
        """
-    pass
+    return a ** b
 
 
 def sqrt(x, n):
     """the function returns the nth root of the x value
        """
-    pass
+    if x < 0:
+        ValueError("sqrtLessThanZero")
+        return
+
+    if x == 0:
+        return 0
+
+    k = n
+    for i in range(100):
+        k = ((n - 1) * (k / n)) + (x / (n * (k ** (n - 1))))
+
+    return k
 
 
 def fact(x):
     """The function returns factorial of x
        """
-    pass
+    if x < 0:
+        ValueError("sqrtLessThanZero")
+        return
+
+    if x == 0:
+        return 0
+
+    k = n
+    for i in range(100):
+        k = ((n - 1) * (k / n)) + (x / (n * (k ** (n - 1))))
+
+    return k
 
 
 def cos(x):
