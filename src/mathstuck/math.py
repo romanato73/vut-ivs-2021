3d16d3e5290177c827f9e66e20b403df9bf9471a
Jakub Vlk <kuba24862vlk@gmail.com>
2021-03-07 01:28:34 +0100

    Fixed fact, sqrt

--- src/mathstuck/math.py
+++ src/mathstuck/math.py
@@ -19,8 +19,7 @@ def div(a, b):
     """The function returns the value a divided by b
        """
     if b == 0:
-        ValueError("DividedByZero")
-        return
+        raise ValueError("DividedByZero")
     return a / b
 
 
@@ -40,11 +39,13 @@ def sqrt(x, n):
     """the function returns the nth root of the x value
        """
     if x < 0:
-        ValueError("sqrtLessThanZero")
-        return
+        raise  ValueError("sqrtLessThanZero")
 
     if x == 0:
         return 0
+
+    if x == 1:
+        return 1
 
     k = n
     for i in range(100):
@@ -61,6 +62,47 @@ def fact(x):
         res *= i
 
     return res
+
+
+def cos(x):
+    """The function returns cosinus of x
+       """
+    xrad = x / 180 * pi()
+    x= 1
+    minus = -1
+    for i in range(2, 150, 2):
+        factr = fact(i)
+        heh = (xrad ** i) / factr
+        x += minus * heh
+        minus *= -1
+
+    return x
+
+
+def sin(x):
+    """The function returns sinus of x
+       """
+    xrad = x / 180 * pi()
+    x= xrad
+    minus = -1
+    for i in range(3, 150, 2):
+        factr = fact(i)
+        heh = (xrad ** i) / factr
+        x += minus * heh
+        minus *= -1
+
+    return x
+
+
+def pi():
+    pi = 3
+    minus = 1
+    for i in range (2, 1000, 2):
+        div = i* (i+1) * (i+2)
+        pi += minus * 4 / div
+        minus *= -1
+
+    return pi
 
 
 def cos(x):
