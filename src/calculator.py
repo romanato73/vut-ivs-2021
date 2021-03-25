bbf897e941d5dbb9faa17e62c6746a1863f1172a
Jakub Vlk <kuba24862vlk@gmail.com>
2021-03-25 22:47:06 +0100

    Add typing numbs on keyboard

--- src/calculator.py
+++ src/calculator.py
@@ -2,6 +2,12 @@ import sys
 from ui import Ui_ivsmath
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.Qt import Qt
+from PyQt5.QtGui import QPen, QBrush
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsItem
+from PyQt5.QtGui import QPen, QBrush, QFont, QColor, QTextCharFormat
+from PyQt5.Qt import Qt, QColor
+
 
 
 class CalculatorWindow(QWidget):
@@ -12,18 +18,71 @@ class CalculatorWindow(QWidget):
         self.ui = Ui_ivsmath()
         self.ui.setupUi(self)
 
-        self.ui.pushButton_1.clicked.connect(self.test)
+        # self.ui.pushButton_1.clicked.connect(self.test)
+
+        transparentcolor = QColor()
+        transparentcolor.setAlpha(0)
+        # pen = QPen()
+        # pen.setColor(transparentcolor)
 
 
+        deffont = QFont("Sans Serif")
+        deffont.setPixelSize(20)
+        deffont.setFamily("Sans Serif")
+
+        textformat = QTextCharFormat()
+        textformat.setFont(deffont)
+        # textformat.setTextOutline(pen)
+
+        self.scene = QGraphicsScene()
+
+        self.scene.setBackgroundBrush(transparentcolor)
+        self.scene.addText("Hovno" , deffont)
+        self.scene.clearSelection()
+        QGraphicsView(self.scene, self)
+
+
+
+    __text__ = ""
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_1:
-            self.prdel()
+            self.appendText("1")
+        if event.key() == Qt.Key_2:
+            self.appendText("2")
+        if event.key() == Qt.Key_3:
+            self.appendText("3")
+        if event.key() == Qt.Key_4:
+            self.appendText("4")
+        if event.key() == Qt.Key_5:
+            self.appendText("5")
+        if event.key() == Qt.Key_6:
+            self.appendText("6")
+        if event.key() == Qt.Key_7:
+            self.appendText("7")
+        if event.key() == Qt.Key_8:
+            self.appendText("8")
+        if event.key() == Qt.Key_9:
+            self.appendText("9")
+        if event.key() == Qt.Key_0:
+            self.appendText("0")
 
-    def test(self):
-        print("presseeeed")
+    def appendText(self, str:str, sep=''):
+        self.__text__ += str
+        if sep == '':
+            self.__text__ += " "
+        QGraphicsView(self.scene, self)
 
-    def prdel(self):
-        print("prdeel")
+    def clearText(self):
+        self.__text__ = ""
+
+    def delText(self):
+        if len(self.__text__) == 1:
+            self.clearText()
+            QGraphicsView(self.scene, self)
+            return
+        self.__text__ = self.text[0:-2]
+        QGraphicsView(self.scene, self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
