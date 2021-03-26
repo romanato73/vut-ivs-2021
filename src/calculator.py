b15ccd96e1bcd02e74e93375a72becd2c3ad0052
AntoninJarolim <jarolim.antonin@seznam.cz>
2021-03-26 16:17:32 +0100

    buttons animations added

--- src/calculator.py
+++ src/calculator.py
@@ -6,8 +6,10 @@ from PyQt5.QtGui import QPen, QBrush
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsItem
 from PyQt5.QtGui import QPen, QBrush, QFont, QColor, QTextCharFormat
-from PyQt5.Qt import Qt, QColor
-
+from PyQt5.Qt import Qt, QColor, pyqtSlot
+import asyncio
+import time
+from PyQt5.QtCore import QTimer
 
 class CalculatorWindow(QWidget):
     def __init__(self):
@@ -38,7 +40,8 @@ class CalculatorWindow(QWidget):
         gpv.setMinimumWidth(400)
         gpv.setAlignment(Qt.AlignLeft)
 
-        self.Button()
+        self.__fading_button = None
+
         self.ui.pushButton_1.clicked.connect(self.button_1)
         self.ui.pushButton_2.clicked.connect(self.button_2)
         self.ui.pushButton_3.clicked.connect(self.button_3)
@@ -52,132 +55,223 @@ class CalculatorWindow(QWidget):
         self.ui.pushButton_add.clicked.connect(self.button_add)
         self.ui.pushButton_sub.clicked.connect(self.button_sub)
         self.ui.pushButton_div.clicked.connect(self.button_div)
+        self.ui.pushButton_mul.clicked.connect(self.button_mul)
         self.ui.pushButton_clear.clicked.connect(self.button_clear)
         self.ui.pushButton_del.clicked.connect(self.button_del)
         self.ui.pushButton_sin.clicked.connect(self.button_sin)
         self.ui.pushButton_cos.clicked.connect(self.button_cos)
-        self.ui.pushButton_fact.clicked.connect(self.button_fact)
+        # nutno pridat tlacitko pro fact, a dat pryc +-
+        #self.ui.pushButton_fact.clicked.connect(self.button_fact)
         self.ui.pushButton_sqrt.clicked.connect(self.button_sqrt)
         self.ui.pushButton_pow.clicked.connect(self.button_pow)
         self.ui.pushButton_pi.clicked.connect(self.button_pi)
         self.ui.pushButton_eq.clicked.connect(self.button_eq)
         self.ui.pushButton_comma.clicked.connect(self.button_decimal)
-
         self.scene.clear()
         self.scene.addText(self.__text__, self.deffont)
 
+    @pyqtSlot()
+    def fade(self):
+        self.__fading_button = self.sender()  # enter the "fading button" state
+        self.__fading_button.setWindowOpacity(0.5)
+        self.__fading_button.setStyleSheet("background-color: #1a1a1a")
+        QTimer.singleShot(75, self.unfade)
+
+    def unfade(self):
+        if self.__fading_button is None:
+            return
+        self.__fading_button.setWindowOpacity(1)
+        self.__fading_button.setStyleSheet("")
+        self.__fading_button = None  # exit the "fading button" state
+
+    def renderText(self):
+        self.scene.clear()
+        self.scene.addText(self.__text__, self.deffont)
+        self.__fading_button = self.sender()
+
     def button_1(self):
+        self.appendText("1")
+        self.fade()        
+        self.renderText() 
         pass
 
     def button_2(self):
+        self.appendText("2")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_3(self):
+        self.appendText("3")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_4(self):
+        self.appendText("4")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_5(self):
+        self.appendText("5")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_6(self):
+        self.appendText("6")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_7(self):
+        self.appendText("7")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_8(self):
+        self.appendText("8")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_9(self):
+        self.appendText("9")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_0(self):
+        self.appendText("0")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_add(self):
+        self.appendText("+")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_sub(self):
+        self.appendText("-")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_div(self):
+        self.appendText("/")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_mul(self):
+        self.appendText("*")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_sin(self):
+        self.appendText("sin(")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_cos(self):
+        self.appendText("cos(")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_del(self):
+        self.clearText()
+        self.fade()
+        self.renderText() 
         pass
 
     def button_clear(self):
+        self.delText()
+        self.fade()
+        self.renderText() 
         pass
 
     def button_pi(self):
+        self.appendText("π")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_sqrt(self):
+        self.appendText("√")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_fact(self):
+        self.appendText("!")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_decimal(self):
+        self.appendText(",")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_pow(self):
+        self.appendText("^")
+        self.fade()
+        self.renderText() 
         pass
 
     def button_eq(self):
+        self.fade()
+        self.renderText() 
         pass
 
     __text__ = ""
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_1:
-            self.appendText("1")
+            self.ui.pushButton_1.click()
         if event.key() == Qt.Key_2:
-            self.appendText("2")
+            self.ui.pushButton_2.click()
         if event.key() == Qt.Key_3:
-            self.appendText("3")
+            self.ui.pushButton_3.click()
         if event.key() == Qt.Key_4:
-            self.appendText("4")
+            self.ui.pushButton_4.click()
         if event.key() == Qt.Key_5:
-            self.appendText("5")
+            self.ui.pushButton_5.click()
         if event.key() == Qt.Key_6:
-            self.appendText("6")
+            self.ui.pushButton_6.click()
         if event.key() == Qt.Key_7:
-            self.appendText("7")
+            self.ui.pushButton_7.click()
         if event.key() == Qt.Key_8:
-            self.appendText("8")
+            self.ui.pushButton_8.click()
         if event.key() == Qt.Key_9:
-            self.appendText("9")
+            self.ui.pushButton_9.click()
         if event.key() == Qt.Key_0:
-            self.appendText("0")
+            self.ui.pushButton_0.click()
         if event.key() == Qt.Key_Backspace:
-            self.delText()
+            self.ui.pushButton_del.click()
         if (event.key() == Qt.Key_Delete) or (event.key() == Qt.Key_C):
-            self.clearText()
-        if (event.key() == Qt.Key_Comma):
-            # desetiná tečka nevím jak se jmenuje key
-            self.appendText(",")
+            self.ui.pushButton_clear.click()
+        if (event.key() == Qt.Key_Comma or event.key() == Qt.Key_Period):
+            self.ui.pushButton_comma.click()
         if event.key() == Qt.Key_Plus:
-            self.appendText("+")
+            self.ui.pushButton_add.click()
         if event.key() == Qt.Key_Minus:
-            self.appendText("-")
+            self.ui.pushButton_sub.click()
         if event.key() == Qt.Key_Slash:
-            self.appendText("/")
+            self.ui.pushButton_div.click()
         if event.key() == Qt.Key_Asterisk:
-            self.appendText("*")
+            self.ui.pushButton_mul.click()
         if event.key() == Qt.Key_Enter:
-            self.parseText()
+            self.ui.pushButton_eq.click()
+
 
         self.scene.clear()
         self.scene.addText(self.__text__, self.deffont)
