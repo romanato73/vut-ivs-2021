05afdffa70067b58a3bbfcf7ab255a8c0c8fad09
Jakub Vlk <kuba24862vlk@gmail.com>
2021-04-24 20:13:28 +0200

    Add comments for documentation of expresionparser.py

--- src/lib/expresionparser.py
+++ src/lib/expresionparser.py
@@ -1,18 +1,22 @@
 from __future__ import division
-from pyparsing import (Literal, CaselessLiteral, Word, Combine, Group, Optional,
-                       ZeroOrMore, Forward, nums, alphas, oneOf, CaselessKeyword, Suppress,Regex )
-import operator
-import sys
+from pyparsing import (Literal, Word, Group, Optional, ZeroOrMore, Forward, nums, alphas, oneOf, CaselessKeyword,
+                       Suppress, Regex)
+
 import re
 
 import mathcore
 
+"""Class for parsing math expresion
+
+"""
+
 
 class MathExpresionParser():
     def __init__(self, ):
-
+        """Initial function. Prepare Literals and other expresion
+            """
         floatnumber = Regex(r"[+-]?\d+(?:\,\d*)?(?:[eE][+-]?\d+)?!*")
-        ident = Word(alphas, alphas + nums + '!'+ "_$")
+        ident = Word(alphas, alphas + nums + '!' + "_$")
 
         plus, minus, mult, div, factorial = map(Literal, "+-*/!")
         lpar, rpar = map(Suppress, "()")
@@ -26,14 +30,9 @@ class MathExpresionParser():
         expr = Forward()
         atom = (Optional(addop) + (ident + lpar + expr + rpar | pi | floatnumber | factorial).setParseAction(
             self.pushFirst)) | Optional(oneOf("- +")) + Group(lpar + expr + rpar).setParseAction(self.pushUMinus)
-        # Definuje jak může vypadát číslo:
-        # 
 
         factor = Forward()
         factor << atom + ZeroOrMore((sqrex + factor).setParseAction(self.pushFirst))
-
-        # factorialc = Forward()
-        # factorialc << factor + ZeroOrMore((floatnumber + factorial).setParseAction(self.pushFirst))
 
         term = factor + ZeroOrMore((multop + factor).setParseAction(self.pushFirst))
 
@@ -41,19 +40,6 @@ class MathExpresionParser():
 
         self.bnf = expr
 
-        """ expresion = infixNotation(
-        operand,
-        [
-            (sinus, 1, opAssoc.RIGHT),
-            (cosinus, 1, opAssoc.RIGHT),
-            ("!", 1, opAssoc.LEFT),
-            ("^", 2, opAssoc.RIGHT),
-            (signop, 1, opAssoc.RIGHT),
-            (multop, 2, opAssoc.LEFT),
-            (plusop, 2, opAssoc.LEFT),
-        ])
-        self.bnf = expresion
-        """
         epsilon = 1e-12
         self.opn = {"+": mathcore.add,
                     "-": mathcore.sub,
@@ -68,13 +54,32 @@ class MathExpresionParser():
                    }
 
     def pushFirst(self, strg, loc, toks):
+        """Append exprStack for 1st element from toks.
+
+        :param toks: elements for parse
+        :param strg: string witch is now parsed
+        :param loc: parser options
+        """
         self.exprStack.append(toks[0])
 
     def pushUMinus(self, strg, loc, toks):
+        """Append exprStack for minus if number is negative.
+
+        :param toks: number for parse
+        :param strg: string witch is now parsed
+        :param loc: parser options
+        """
+
         if toks and toks[0] == '-':
             self.exprStack.append('unary -')
 
     def eval(self, num_string, parseAll=True):
+        """Quantification of num_string.
+
+        :param num_string: string with parsed numbers
+
+        :return quantification number
+        """
         self.exprStack = []
         results = self.bnf.parseString(num_string, parseAll)
         print(results)
@@ -82,6 +87,12 @@ class MathExpresionParser():
         return val
 
     def evaluateStack(self, s):
+        """Quantification of s by declared operation.
+
+        :param s: string witch is parsed
+
+        :return quantification number
+        """
         print(s)
         op = s.pop()
         if op == 'unary -':
@@ -104,5 +115,4 @@ class MathExpresionParser():
         elif op[0].isalpha():
             return 0
         else:
-            return float(op.replace(',','.'))
-
+            return float(op.replace(',', '.'))
